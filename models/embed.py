7906f4675877836d76190cbd55c895d0450ab9d9
Harit Tarwani <112678530+HaritMT25@users.noreply.github.com>
2025-02-07 14:33:28 +0530

    Update embed.py

--- models/embed.py
+++ models/embed.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 
 class TokenEmbedding(nn.Module):
-    def __init__(self, c_in, d_model, tao=24, m=7, pad=True, is_split=True):
+    def __init__(self, c_in, d_model, tao=1, m=0, pad=True, is_split=True):
         super(TokenEmbedding, self).__init__()
         self.tao = tao
         self.m = m
@@ -191,7 +191,7 @@ class DataEmbedding(nn.Module):
         dropout: dropout rate.
         """
         super(DataEmbedding, self).__init__()
-        self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model, m=7, tao=24)
+        self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model, m=0, tao=1)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
         print('Hey_this_is_our_code', flush=True)
         if embed_type != 'timeF':
